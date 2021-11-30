1953922dceb2fd0db03403eb80a92ec02b705073
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-11-30 13:07:02 -0800

    Finished mvp

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -102,5 +102,23 @@ router.delete('/:id', async (req, res) => {
         })
 })
 //get comments by post id
+router.get('/:id/comments', (req, res) => {
+    const { id } = req.params
+    Post.findById(req.params.id)
+      .then(post => {
+        if(!post){
+            res.status(404).json({ message: "The post with the specified ID does not exist" });
+        }
+        else{
+            Post.findPostComments(id)
+                .then(comments => {
+                    res.status(200).json(comments);
+                })
+                .catch(error => {
+                    res.status(500).json({ message: "The comments information could not be retrieved" });
+                });
+        }
+      }) 
+  });
 
  module.exports = router
