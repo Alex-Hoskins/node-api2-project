355ad9c537205f8e4648ce10dbed6d8e05167ae2
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-11-30 11:55:32 -0800

    get all posts and get post by id done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,39 @@
 // implement your posts router here
+const express = require('express')
+const Post = require('./posts-model')
+const router = express.Router()
+
+// get all posts
+router.get('/', (req, res) => {
+    Post.find()
+      .then(posts => {
+        res.status(200).json(posts);
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ message: "The posts information could not be retrieved" });
+      });
+  });
+
+//get post by id
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if(!post){
+            res.status(404).json({ message: "The post with the specified ID does not exist" });
+        }
+        else{
+            res.status(200).json(post);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ message: "The post information could not be retrieved" });
+      });
+  });
+//post to posts
+//put post by id
+//delete by id
+//get comments by post id
+
+ module.exports = router
